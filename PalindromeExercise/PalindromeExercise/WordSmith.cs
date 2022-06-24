dfb36756b2e9b645658ea395cc3d99b34352f7fd
David Chuvik <dchuvik@gmail.com>
2022-06-24 12:09:39 -0400

    Made a Palendrome test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            return word.SequenceEqual(word.Reverse())==true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("repaper", true)]
+        [InlineData("David", false)]
+        [InlineData("TrueCoders", false)]
 
+
+        public void Test1(string word, bool expected)
+        {
+            //arrange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
